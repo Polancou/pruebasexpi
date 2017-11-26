05f5f05851ecf862f05df433bb002accd5e0d495
ramseseduardom@gmail.com <RamsesMtnz@192.168.1.69>
2017-11-26 12:27:20 -0600

    correccion las imagenes, errores notificados en la prueba con alumnos

--- src/java/sys/bean/beanReportes.java
+++ src/java/sys/bean/beanReportes.java
@@ -5,33 +5,24 @@
  */
 package sys.bean;
 
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperRunManager;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
 
 /**
  *
@@ -41,113 +32,110 @@ import org.primefaces.model.StreamedContent;
 @RequestScoped
 public class beanReportes implements Serializable {
 
-    private StreamedContent file;
-    private List<sys.model.Alumnos> alumnos=new ArrayList <sys.model.Alumnos>();
-    private List<sys.model.AreaTratamientos> areaTratamientos = new ArrayList <sys.model.AreaTratamientos>();
-    private List<sys.model.BitacoraRecibos> bitacora = new ArrayList <sys.model.BitacoraRecibos>();
+    /**
+     * @return the clinica
+     */
+    public String getClinica() {
+        return clinica;
+    }
+
+    /**
+     * @param clinica the clinica to set
+     */
+    public void setClinica(String clinica) {
+        this.clinica = clinica;
+    }
+
+    /**
+     * @return the fecha1
+     */
+    public Date getFecha1() {
+        return fecha1;
+    }
+
+    /**
+     * @param fecha1 the fecha1 to set
+     */
+    public void setFecha1(Date fecha1) {
+        this.fecha1 = fecha1;
+    }
+
+    /**
+     * @return the fecha2
+     */
+    public Date getFecha2() {
+        return fecha2;
+    }
+
+    /**
+     * @param fecha2 the fecha2 to set
+     */
+    public void setFecha2(Date fecha2) {
+        this.fecha2 = fecha2;
+    }
+
+    private String clinica;
+    private Date fecha1;
+    private Date fecha2;
     
-    
-    /**
-     * @return the alumnos
-     */
-    public List<sys.model.Alumnos> getAlumnos() {
-        return alumnos;
+    public void reporteGeneral() throws SQLException, IOException, JRException{
+        exportarPDF("/resources/ReportesPDF/generalNew.jasper","Reporte_Gral_Todas_Las_Clinicas_Desde_La_Implementacion",null);
     }
 
-    /**
-     * @param alumnos the alumnos to set
-     */
-    public void setAlumnos(List<sys.model.Alumnos> alumnos) {
-        this.alumnos = alumnos;
-    }
-
-    /**
-     * @return the areaTratamientos
-     */
-    public List<sys.model.AreaTratamientos> getAreaTratamientos() {
-        return areaTratamientos;
-    }
-
-    /**
-     * @param areaTratamientos the areaTratamientos to set
-     */
-    public void setAreaTratamientos(List<sys.model.AreaTratamientos> areaTratamientos) {
-        this.areaTratamientos = areaTratamientos;
-    }
-
-    /**
-     * @return the bitacora
-     */
-    public List<sys.model.BitacoraRecibos> getBitacora() {
-        return bitacora;
-    }
-
-    /**
-     * @param bitacora the bitacora to set
-     */
-    public void setBitacora(List<sys.model.BitacoraRecibos> bitacora) {
-        this.bitacora = bitacora;
-    }
-
-    
-    public StreamedContent getFile() {
-        return file;
-    }
-
-    public beanReportes() {
-        InputStream stream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/images/fdo.png");
-        file = new DefaultStreamedContent(stream, "image/png", "bita.png");
-        System.out.print("Entra a metodo reporte general");
+    public void reportePorClinica() throws SQLException, IOException, JRException {
+        System.out.print("+++++");
+        Map <String,Object> parametros = new HashMap<String, Object>();
+        parametros.put("clinica", clinica);
+        System.out.print("+++++"+clinica);
+        //exportarPDF("/resources/ReportesPDF/soloClinica.jasper","Reporte_De_La_Clinica_"+clinica+"_Desde_La_Implementacion",  parametros);
     }
     
-    public void exportarPDF() throws SQLException, IOException, JRException {
+    public void reporteClinicasPorFechas() throws IOException, SQLException, JRException {
+        System.out.print("+++++");
+        Map <String,Object> parametros = new HashMap<String, Object>();
+        parametros.put("fecha1", fecha1);
+        parametros.put("fecha2", fecha2);
+        System.out.print("+++++ "+fecha1+" +++++ "+fecha2);
+        //exportarPDF("/resources/ReportesPDF/soloClinica.jasper","Reporte_De_La_Clinica_"+clinica+"_Desde_La_Implementacion",  parametros);        
+    }
+    
+    public void reportePorClinicaPorFechas(){
+        System.out.print("+++++");
+        Map <String,Object> parametros = new HashMap<String, Object>();
+        parametros.put("clinica", clinica);
+        parametros.put("fecha1", fecha1);
+        parametros.put("fecha2", fecha2);
+        System.out.print("+++++ "+fecha1+" +++++ "+fecha2+" ++++++ "+clinica);
+        //exportarPDF("/resources/ReportesPDF/soloClinica.jasper","Reporte_De_La_Clinica_"+clinica+"_Desde_La_Implementacion",  parametros);        
+    }
+    
+    public void alumnosDestacados(){
+        Map <String,Object> parametros = new HashMap<String, Object>();
+        parametros.put("clinica", clinica);
+        parametros.put("fecha1", fecha1);
+        parametros.put("fecha2", fecha2);
+        System.out.print("+++++ "+fecha1+" +++++ "+fecha2+" ++++++ "+clinica);
+        //exportarPDF("/resources/ReportesPDF/soloClinica.jasper","Reporte_De_La_Clinica_"+clinica+"_Desde_La_Implementacion",  parametros);        
+    }
+    
+    public void ejemplo(){
+        System.out.print("que pedo si entra....");
+    }
+
+    public void exportarPDF(String rutaArchivo,String nombreArchivo,Map parametros) throws SQLException, IOException, JRException {
         //Map <String,Object> parametros = new HashMap<String, Object>();
         //parametros.put("", "");
-//      
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/facultad_odontologia", "root", "root");
+        File jasperReport=new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath(rutaArchivo));
+        JasperPrint jasperPrint=JasperFillManager.fillReport(jasperReport.getPath(), parametros, connection);
         HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-//        
-        ServletOutputStream servletOutputStream = response.getOutputStream();
-		File reportFile = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/ReportesPDF/generalNew.jasper"));
-		
-		byte[] bytes = null;
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/facultad_odontologia","root","root");
-                
-                try {
-				 bytes = JasperRunManager.runReportToPdf(reportFile.getPath(), null, connection);
-				//beanEstatal.getModel().setBytesFormato(bytes);	
-				 response.setContentType("application/pdf");
-					response.setContentLength(bytes.length);
-					servletOutputStream.write(bytes, 0, bytes.length);
-					servletOutputStream.flush();
-					servletOutputStream.close();
-			}finally{
-				connection.close();
-			}	
+        response.addHeader("Content-disposition", "attachment; filename="+nombreArchivo+".pdf");
+        ServletOutputStream stream = response.getOutputStream();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
+        stream.flush();
+        stream.close();
+        FacesContext.getCurrentInstance().responseComplete();
     }
 
-    public void exportarPDFClinica() throws SQLException, IOException, JRException {
-//        Map <String,Object> parametros = new HashMap<String, Object>();
-//        parametros.put("clinica", );
-////      
-        HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-//        
-        ServletOutputStream servletOutputStream = response.getOutputStream();
-		File reportFile = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/ReportesPDF/generalNew.jasper"));
-		
-		byte[] bytes = null;
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/facultad_odontologia","root","root");
-                
-                try {
-				 bytes = JasperRunManager.runReportToPdf(reportFile.getPath(), null, connection);
-				//beanEstatal.getModel().setBytesFormato(bytes);	
-				 response.setContentType("application/pdf");
-					response.setContentLength(bytes.length);
-					servletOutputStream.write(bytes, 0, bytes.length);
-					servletOutputStream.flush();
-					servletOutputStream.close();
-			}finally{
-				connection.close();
-			}	
-    }
-    
+
 }
